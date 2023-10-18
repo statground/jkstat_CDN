93f87cd2322044c6ccbc73d3c289c2ffe7a5c39a
Jaeseong Yoo <praster1@gmail.com>
2023-10-18 18:20:06 +0900

    Div_footer

---
+++ scripts/common/footer/Div_footer.js
@@ -0,0 +1,23 @@
+function Div_footer() {
+	return (
+		<div class="flex flex-col w-full px-[100px] py-[10px] md:px-[10px]">
+			<div class="flex flex-row md:flex-col md:items-center md:justify-between">
+				<span class="text-xl font-bold">
+					JK통계컨설팅<br/>
+				</span>
+
+				<span class="text-sm text-end py-[10px] md:text-start md:px-[0px]">
+					사업자등록번호: 000-00-00000<br/>
+					주소<br/>
+					대표전화: 000-0000-0000　|　문의: email@testemail.com
+				</span>
+			</div>
+			
+			<div class="flex flex-row w-full justify-center items-center text-center py-2">
+				<span class="text-sm">
+					Copyright © 2023 JK통계컨설팅 All rights reserved.
+				</span>
+			</div>
+		</div>    
+	)
+}
