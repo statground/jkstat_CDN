1aa7ea66571ed988b7f92eaad0b63727dc45b3fa
Jaeseong Yoo <praster1@gmail.com>
2023-11-01 02:05:47 +0900

    footer에서 사업자등록번호 제거

---
+++ scripts/common/footer/Div_footer_20231101_0205.js
@@ -0,0 +1,11 @@
+function Div_footer() {
+	return (
+		<div class="flex flex-col w-full px-[100px] py-[10px] md:px-[10px]">
+			<div class="flex flex-row w-full justify-center items-center text-center py-2">
+				<span class="text-sm">
+					Copyright © 2023 JK통계컨설팅 All rights reserved.
+				</span>
+			</div>
+		</div>    
+	)
+}
