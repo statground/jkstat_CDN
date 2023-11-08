500833f90f18cbd29854c7c38d14bdf01b7da836
Jaeseong Yoo <praster1@gmail.com>
2023-11-09 06:15:42 +0900

    블로그

---
+++ scripts/news/Div_main.js
@@ -0,0 +1,118 @@
+function Div_main() {
+	return (
+		<aside aria-label="Related articles" class="py-8 lg:py-24 bg-white antialiased">
+			<div class="px-4 mx-auto max-w-screen-xl space-y-8">
+
+				<nav class="flex mb-4" aria-label="Breadcrumb">
+					<ol class="inline-flex items-center space-x-1 md:space-x-3">
+						<li class="inline-flex items-center">
+							<a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600">
+								<svg class="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+									<path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+								</svg>
+								<a href="/" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2">
+									Home
+								</a>
+							</a>
+						</li>
+						<li>
+							<div class="flex items-center">
+								<svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+								</svg>
+								<span class="ml-1 text-sm font-medium text-gray-500 md:ml-2">
+									소식
+								</span>
+							</div>
+						</li>
+						<li aria-current="page">
+							<div class="flex items-center">
+								<svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+								</svg>
+								<span class="ml-1 text-sm font-medium text-gray-500 md:ml-2">
+									블로그
+								</span>
+							</div>
+						</li>
+					</ol>
+				</nav>
+				
+				<h2 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl">
+					JK 통계 컨설팅 블로그
+				</h2>
+				  
+				<article class="flex mb-8">
+					<a href="#" class="shrink-0">
+						<img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" class="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1" />
+					</a>
+					<div class="flex flex-col justify-center">
+						<h2 class="mb-2 text-xl font-bold leading-tight text-gray-900">
+							<a href="#">Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 </a>
+						</h2>
+						<p class="mb-2 text-gray-500">
+							Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 
+						</p>
+						
+					</div>
+				</article>
+				<article class="flex mb-8">
+					<a href="#" class="shrink-0">
+						<img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" class="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1" />
+					</a>
+					<div class="flex flex-col justify-center">
+						<h2 class="mb-2 text-xl font-bold leading-tight text-gray-900">
+							<a href="#">Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 </a>
+						</h2>
+						<p class="mb-2 text-gray-500">
+							Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 
+						</p>
+						
+					</div>
+				</article>
+				<article class="flex mb-8">
+					<a href="#" class="shrink-0">
+						<img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" class="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1" />
+					</a>
+					<div class="flex flex-col justify-center">
+						<h2 class="mb-2 text-xl font-bold leading-tight text-gray-900">
+							<a href="#">Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 </a>
+						</h2>
+						<p class="mb-2 text-gray-500">
+							Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 
+						</p>
+						
+					</div>
+				</article>
+				<article class="flex mb-8">
+					<a href="#" class="shrink-0">
+						<img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" class="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1" />
+					</a>
+					<div class="flex flex-col justify-center">
+						<h2 class="mb-2 text-xl font-bold leading-tight text-gray-900">
+							<a href="#">Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 </a>
+						</h2>
+						<p class="mb-2 text-gray-500">
+							Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 
+						</p>
+						
+					</div>
+				</article>
+				<article class="flex mb-8">
+					<a href="#" class="shrink-0">
+						<img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" class="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full" alt="Image 1" />
+					</a>
+					<div class="flex flex-col justify-center">
+						<h2 class="mb-2 text-xl font-bold leading-tight text-gray-900">
+							<a href="#">Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 </a>
+						</h2>
+						<p class="mb-2 text-gray-500">
+							Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 Contents 1 
+						</p>
+						
+					</div>
+				</article>
+			</div>
+		</aside>
+	)
+}
